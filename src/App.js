7cf101145139b4f7cf9fda38459ae0fb0e2cb9ff
Isaac Andela <dbxeindbaas@isaacandela.nl>
2017-04-12 13:43:52 +0200

    cleaning up and some more graph experimenting

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import axios from 'axios';
 import {Stats} from './Stats';
+import {injectGlobal} from 'styled-components';
 
-const apiUrl = 'http://opendata.cbs.nl/ODataApi/odata/82242NED/TypedDataSet?$top=10&$filter=((TypeGefailleerde+eq+%27TG08%27))+and+((substringof(%27MM%27,Perioden)))';
+const datasetUri = `http://opendata.cbs.nl/ODataApi/odata/82242NED/TypedDataSet`;
+const datasetFilter = `$filter=((TypeGefailleerde+eq+%27TG08%27))+and+((substringof(%27MM%27,Perioden)))`;
+const limit = 1000;
 
+const apiUri = `${datasetUri}?$top=${limit}&${datasetFilter}`;
 
+injectGlobal`
+  html {
+    font-family: sans-serif;
+  }
+`;
 
-export const App = class App extends Component {
+export const App = class App extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {};
 
-    axios.get(apiUrl).then(({data}) => this.setState((prevState) => Object.assign({}, prevState, data)));
+    axios.get(apiUri).then(({data}) => this.setState((prevState) => Object.assign({}, prevState, data)));
   }
   render() {
     return (
--- src/Stats.js
+++ src/Stats.js
@@ -1,9 +1,58 @@
 import React from 'react';
-import {VictoryBar, VictoryChart, VictoryAxis} from 'victory';
+import {VictoryChart, VictoryLine, VictoryTooltip, VictoryAxis} from 'victory';
+import styled, {keyframes} from 'styled-components';
+const hasData = ({data}) => data !== null && data !== undefined;
 
-export const Stats = ({data}) => 
-    <VictoryChart domainPadding={20}>
-        <VictoryAxis dependentAxis fixLabelOverlap/>
-        <VictoryAxis fixLabelOverlap />
-        <VictoryBar data={data} x="Perioden" y="UitgesprokenFaillissementen_1"/>
-    </VictoryChart>;
+const fadeIn = keyframes`
+    from {
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+    }
+`;
+
+const StatsContainer = styled.div`
+    animation: ${fadeIn} 0.3s ease-in;
+`;
+
+const RenderStats = ({data}) => (
+    <StatsContainer>
+        <VictoryChart domainPadding={0}>
+            <VictoryAxis dependentAxis fixLabelOverlap/>
+            <VictoryAxis
+                fixLabelOverlap
+                style={{
+                    ticks: {stroke: "black", strokeWidth: 2, size: 2},
+                    grid: {stroke: "grey"},
+                }}
+            />
+            <VictoryLine
+                data={data}
+                x="Perioden"
+                y="UitgesprokenFaillissementen_1"
+                labels={({UitgesprokenFaillissementen_1, Perioden}) => `${UitgesprokenFaillissementen_1} in ${Perioden}`}
+                labelComponent={<VictoryTooltip/>}
+                interpolation="linear"
+                style={{
+                    data: {
+                        strokeWidth: 1,
+                    }
+                }}
+            />
+        </VictoryChart>
+    </StatsContainer>
+);
+
+const Message = styled.p`
+    opacity: 0;
+    animation: ${fadeIn} 0.2s ease-in 0.2s forwards;
+`;
+
+const RenderLoadingStats = () => (
+    <Message>
+        {`Een moment gedult AUB`}
+    </Message>
+);
+
+export const Stats = (props) => hasData(props) ? RenderStats(props) : RenderLoadingStats(props);
